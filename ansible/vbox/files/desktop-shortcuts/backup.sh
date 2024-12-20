93cc49f93dbcc253853798a817510e232b887dca
Myko <minv5725495@gmail.com>
2024-12-20 02:03:42 +0200

    add wallpaper

--- ansible/vbox/files/desktop-shortcuts/backup.sh
+++ ansible/vbox/files/desktop-shortcuts/backup.sh
@@ -1,6 +1,7 @@
 mkdir -p /media/sf_VBoxShared/${HOSTNAME};
 cd ~;
 tar -czf /media/sf_VBoxShared/${HOSTNAME}/webbkp-`date -I`.tgz \
+.ssh/* \
 .electrum/* \
 .mozilla/firefox/* \
 snap/firefox/common/.mozilla/firefox/* \
